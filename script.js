bf7c3aac2ee1a274c5befcaa5fcffb7c31120677
Mirllan <edilsonmirllan038@gmail.com>
2026-05-20 11:34:25 -0500

    a

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let filteredData = [];
 let currentChannel = '';
 
 // URL base de la API
-const API_BASE_URL = 'https://levapan-production.up.railway.app/api/Levapan/pdv';
+const API_BASE_URL = 'https://levapan-production.up.railway.app/api/natural/pdv';
 
 // Cargar datos de la API según el canal seleccionado
 async function loadData() {
